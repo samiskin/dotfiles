feee8e01f0361981d70b80d3d1f69ffcd925feae
Shiranka <shiranka.miskin@gmail.com>
2015-12-02 21:30:39 -0800

    Added install script and zsh theme

---
+++ install.sh
@@ -0,0 +1,38 @@
+# Save script's current directory
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+cp ./.bashrc ~/
+cp ./bash_profile ~/
+
+
+# Install homebrew if its not installed
+which -s brew
+if [[ $? != 0 ]] ; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  brew update
+fi
+
+# Install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Vundle Plugins
+cp ./vimrc ~/
+vim +PluginInstall +qall
+~/.vim/bundle/YouCompleteMe/install.py
+
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+chsh -s $(which zsh); ok
+
+cp ./remyagnoster.zsh-theme ~/.oh-my-zsh/themes/
+
+# Install Tmux
+brew install tmux
+cp ./.tmux.conf ~/
